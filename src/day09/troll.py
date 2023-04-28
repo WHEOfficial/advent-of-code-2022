cdba7d480848e617e9e477da789c5319534e7f43
WHEOfficial <46579241+WHEOfficial@users.noreply.github.com>
2023-04-28 14:11:21 -0400

    Refactored Day 09 code and added visualize func

--- src/day09/troll.py
+++ src/day09/troll.py
@@ -9,81 +9,61 @@ dirs = {
 
 sign = lambda x: int(math.copysign(1, x))
 
-def visualize_rope(head, tail, dims):
-    for y in range(dims[1] - 1, -1, -1):
+def visualize_rope(rope, dims, start=[0, 0]):
+    for y in range(dims[1] - 1, start[1] - 1, -1):
         row = ''
-        for x in range(dims[0]):
-            if (x, y) == head:
-                row += 'H'
-            elif (x, y) == tail:
-                row += 'T'
-            else:
-                row += '.'
+        for x in range(start[0], dims[0]):
+            found = False
+            for i, k in enumerate(rope):
+                if k == [x, y]:
+                    row += 'H' if i == 0 else str(i)
+                    found = True
+                    break
+            row += '.' if not found else ''
+
         print(row)
     print()
 
+def simulate_rope(knots, visualize=False):
+    rope = [[0, 0] for i in range(knots + 1)]
+    tail_visits = [[0, 0]]
+    
+    for l in lines:
+        step = l.split(' ')
+        dir_letter, num = step[0], int(step[1])
+        dir = dirs[dir_letter]
+
+        for i in range(num):
+            rope[0][0] += dir[0]
+            rope[0][1] += dir[1]
+
+            for i in range(1, knots + 1):
+                x_diff, y_diff = rope[i-1][0] - rope[i][0], rope[i-1][1] - rope[i][1]
+
+                if abs(x_diff) > 1 and y_diff == 0:
+                    rope[i][0] += sign(x_diff)
+                elif abs(y_diff) > 1 and x_diff == 0:
+                    rope[i][1] += sign(y_diff)
+                elif abs(x_diff) > 1 and abs(y_diff) > 0 or \
+                    abs(y_diff) > 1 and abs(x_diff) > 0:
+                    rope[i][0] += sign(x_diff)
+                    rope[i][1] += sign(y_diff)
+            
+            if rope[knots] not in tail_visits:
+                tail_visits.append(rope[knots].copy())
+            
+            if visualize:
+                visualize_rope(rope, [10, 10], [-10, -10])
+    
+    return len(tail_visits)
+
 with open("data/day09.txt", 'r') as infile:
     lines = infile.read().splitlines()
-    part1 = False
+    part1 = True
     part2 = True
 
     if part1:
-        head_pos = [0, 0]
-        tail_pos = [0, 0]
-        tail_visits = [[0, 0]]
-
-        for l in lines:
-            step = l.split(' ')
-            dir_letter, num = step[0], int(step[1])
-            dir = dirs[dir_letter]
-
-            for i in range(num):
-                head_pos[0] += dir[0]
-                head_pos[1] += dir[1]
-
-                x_diff, y_diff = head_pos[0] - tail_pos[0], head_pos[1] - tail_pos[1]
-
-                if abs(x_diff) > 1 and y_diff == 0:
-                    tail_pos[0] += sign(x_diff)
-                elif abs(y_diff) > 1 and x_diff == 0:
-                    tail_pos[1] += sign(y_diff)
-                elif abs(x_diff) > 1 and abs(y_diff) > 0 or \
-                    abs(y_diff) > 1 and abs(x_diff) > 0:
-                    tail_pos[0] += sign(x_diff)
-                    tail_pos[1] += sign(y_diff)
-                
-                if tail_pos not in tail_visits:
-                    tail_visits.append(tail_pos.copy())
-
-        print(len(tail_visits))
+        print(simulate_rope(1))
     
     if part2:
-        knots = 9
-        rope = [[0, 0] for i in range(knots + 1)]
-        tail_visits = [[0, 0]]
-        
-        for l in lines:
-            step = l.split(' ')
-            dir_letter, num = step[0], int(step[1])
-            dir = dirs[dir_letter]
-
-            for i in range(num):
-                rope[0][0] += dir[0]
-                rope[0][1] += dir[1]
-
-                for i in range(1, knots + 1):
-                    x_diff, y_diff = rope[i-1][0] - rope[i][0], rope[i-1][1] - rope[i][1]
-
-                    if abs(x_diff) > 1 and y_diff == 0:
-                        rope[i][0] += sign(x_diff)
-                    elif abs(y_diff) > 1 and x_diff == 0:
-                        rope[i][1] += sign(y_diff)
-                    elif abs(x_diff) > 1 and abs(y_diff) > 0 or \
-                        abs(y_diff) > 1 and abs(x_diff) > 0:
-                        rope[i][0] += sign(x_diff)
-                        rope[i][1] += sign(y_diff)
-                
-                if rope[knots] not in tail_visits:
-                    tail_visits.append(rope[knots].copy())
-    
-    print(len(tail_visits))
+        print(simulate_rope(9))
